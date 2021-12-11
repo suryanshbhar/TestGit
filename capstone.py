1ffd49160e3e21cc557f2de9ef3c81158d422762
suryansh <suryanshbhar@gmail.com>
2021-12-11 12:27:01 +0530

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -7,6 +7,11 @@ import board
 import busio
 import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
+from gpiozero import Buzzer
+from time import sleep
+
+
+buzzer = Buzzer(17)
 
 
 firebase = firebase.FirebaseApplication('https://capstone-7efdb-default-rtdb.firebaseio.com/', None)
@@ -25,9 +30,12 @@ chan = AnalogIn(ads,ADS.P0)
 man = AnalogIn(ads,ADS.P1)
 ban = AnalogIn(ads,ADS.P2)
 
+print("Initializing Sensors")
+
 
 while True:
     time.sleep(5)
+    print("Gathering Sensor Data")
     try:
         temperature = sensor.get_temperature()
     except:
@@ -50,7 +58,41 @@ while True:
         tds=(133.42*v*v*v -255.86*v*v+857.39*v)*0.5
     except:
         tds="error"
-    print("The ntu units are %s" % tds)
+    print("The tds units are %s" % tds)
+
+    flag = 0
+
+    if(temperature > 40 or temperature< 5){
+        print("TEMPERATURE ALERT !")
+        flag=1
+    }
+    if(ph<4 or ph>9){
+        print("pH ALERT !")
+        flag=1
+    }
+    if(ntu>350 or ntu<0){
+        print("TURBIDITY ALERT !")
+        flag=1
+    }
+    if(tds>380){
+        print("TDS sensor ALERT !")
+        flag=1
+    }
+
+    if(flag = 1){
+        buzzer.on()
+    }else{
+        buzzer.off()
+        print("Water Quality Appropiate")
+    }
+
+
+
+
+
+
+
+    print('Sending data....')
     
     #data transmission
     now = datetime.now()
@@ -63,3 +105,5 @@ while True:
         'time': ctime,
     }
     result = firebase.post(f'/{username}/',data)
+    print('Data sent successfully')
+    print('-------------------------------------------------------------------------------------------')
