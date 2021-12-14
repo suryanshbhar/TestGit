54b62cc92e187e929f28ab0a1566e33ea38e59f0
suryansh <suryanshbhar@gmail.com>
2021-12-14 16:59:02 +0530

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -30,27 +30,36 @@ chan = AnalogIn(ads,ADS.P0)
 man = AnalogIn(ads,ADS.P1)
 ban = AnalogIn(ads,ADS.P2)
 
-print("Initializing Sensors")
+print("Initializing Sensors......")
 
 
 while True:
     time.sleep(5)
+    
+    temf=0
+    turbf=0
+    tdsf=0
+    phf=0
+    
     print("Gathering Sensor Data")
     try:
         temperature = sensor.get_temperature()
     except:
         temperature="error"
+        temf=1
     print("The temperature is %s celsius" % temperature)
     try:
         ph = 3.28*chan.voltage+1.28
     except:
         ph="error"
+        phf=1
     print("The pH is %s" % ph)
     
     try:
-        ntu = (-118.4*man.voltage) + 461.15
+        ntu = (-118.1*man.voltage) + 461.15
     except:
         ntu="error"
+        turbf=1
     print("The ntu units are %s" % ntu)
     
     try:
@@ -58,37 +67,46 @@ while True:
         tds=(133.42*v*v*v -255.86*v*v+857.39*v)*0.5
     except:
         tds="error"
+        tdsf=1
     print("The tds units are %s" % tds)
 
     flag = 0
+    
+    
 
-    if(temperature > 40 or temperature< 5){
+    if (temf!=1 and (temperature > 40 or temperature< 5) ) :
         print("TEMPERATURE ALERT !")
         flag=1
-    }
-    if(ph<4 or ph>9){
+    
+    if (phf!=1 and (ph<4 or ph>9)):
         print("pH ALERT !")
         flag=1
-    }
-    if(ntu>350 or ntu<0){
+    
+    if (turbf!=1 and (ntu>350 )):
         print("TURBIDITY ALERT !")
         flag=1
-    }
-    if(tds>380){
+    
+    if (tdsf!=1 and (tds>380)):
         print("TDS sensor ALERT !")
         flag=1
-    }
+        
+    if temf or phf or turbf or tdsf:
+        print('Errors detected in sensors !')
+    
 
-    if(flag = 1){
+    if (flag == 1):
         buzzer.on()
-    }else{
+        time.sleep(5)
+        print('ML prediction 0')
+        print('Treatment Motor ON')
+    
+    else:
         buzzer.off()
         print("Water Quality Appropiate")
-    }
-
-
-
-
+        time.sleep(2)
+        print('ML prediction 1')
+        print('Treatment Motor OFF')
+    
 
 
 
@@ -105,5 +123,6 @@ while True:
         'time': ctime,
     }
     result = firebase.post(f'/{username}/',data)
+    time.sleep(2)
     print('Data sent successfully')
     print('-------------------------------------------------------------------------------------------')
